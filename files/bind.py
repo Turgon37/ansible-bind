8494b4d314b9af9fd86aa82933a040f0590ab433
Pierre GINDRAUD <pgindraud@gmail.com>
2017-11-10 21:36:52 +0100

    fix fact script

--- files/bind.py
+++ files/bind.py
@@ -8,13 +8,13 @@ import sys
 
 content=dict()
 
-version_re = re.compile('^BIND\s+(?P<version>(?P<major>[0-9]+)[^ ]+).$')
+version_re = re.compile('^BIND\s+(?P<version>(?P<major>[0-9]+)[^ ]+)')
 try:
     result = subprocess.Popen(['/usr/bin/env', 'named', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     (stdout, stderr) = result.communicate()
     output = stdout + stderr
-    for line in output.split():
-        match = version_re.search(line)
+    for line in output.split('\n'):
+        match = version_re.search(line.strip())
         if match:
             content['version_full'] = match.group('version')
             content['version_major'] = match.group('major')
