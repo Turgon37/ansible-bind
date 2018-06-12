6cdd49ed3a21cbaf07c69b625386111cb082ef67
Pierre GINDRAUD <pgindraud@gmail.com>
2018-06-12 21:23:51 +0200

    add dns test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_rrentry(host):
+    assert 'localhost.' in host.check_output('dig +noall +answer NS test.local @localhost')
+    assert '192.168.1.1' in host.check_output('dig +noall +answer A server1.test.local @localhost')
+    assert 'web-server' in host.check_output('dig +noall +answer CNAME www.test.local @localhost')
