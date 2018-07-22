d04d7ee325cbf3a4a63ce6b9138fd53e14f1a5f6
Pierre GINDRAUD <pgindraud@gmail.com>
2018-07-22 16:28:38 +0200

    typo

--- files/bind.fact.py
+++ files/bind.fact.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 import json
-import os
 import re
 import subprocess
 import sys
 
-content=dict()
+content = dict()
 
 version_re = re.compile('^BIND\s+(?P<version>(?P<major>[0-9]+)[^ ]+)')
 try:
-    result = subprocess.Popen(['/usr/bin/env', 'named', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    result = subprocess.Popen(['/usr/bin/env', 'named', '-v'],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              universal_newlines=True)
     (stdout, stderr) = result.communicate()
     output = stdout + stderr
     for line in output.split('\n'):
